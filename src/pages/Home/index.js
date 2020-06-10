2c5f63376b2337cd3d7334e7ca976d58444af949
RichardMathew <richardmathewsa.silva@hotmail.com>
2020-06-10 20:33:18 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import { ProductList } from './styles';
+import api from '../../services/api';
+import * as CartActions from '../../store/modules/cart/action'
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const amount = useSelector(state => state.cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount;
+
+    return sumAmount;
+    }, {})
+  );
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    async function loadPorducts() {
+      const response = await api.get('products');
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(data);
+    }
+    loadPorducts();
+  });
+
+  function handleAddProduct(id) {
+    dispatch(CartActions.addToCartRequest(id));
+  };
+
+  return (
+    <ProductList>
+      { products.map(product => (
+        <li key={product.id}>
+          <img
+            src={product.image}
+            alt={product.title}
+            />
+
+          <div>
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+          </div>
+
+          <button
+            type="button"
+            onClick={() => handleAddProduct(product.id)}
+          >
+
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" />{' '}
+                {amount[product.id] || 0}
+            </div>
+
+          <span>ADCIONAR AO CARRINHO</span>
+        </button>
+        </li>
+      ))}
+    </ProductList>
+  );
+}
